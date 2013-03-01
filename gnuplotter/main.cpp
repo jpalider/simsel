1515b9e6eb7cc077e05a772463adeb0cb09220f2
Jakub Palider <jpalider@gmail.com>
2013-03-01 13:34:04 +0100

    Adding command line options for gnuplotter cont.

--- gnuplotter/main.cpp
+++ gnuplotter/main.cpp
@@ -5,6 +5,52 @@
 #include "GNUPlotter.h"
 #include "optionparser.h"
 
+// struct Arg: public option::Arg
+// {
+//   static void printError(const char* msg1, const option::Option& opt, const char* msg2)
+//   {
+//     fprintf(stderr, "%s", msg1);
+//     fwrite(opt.name, opt.namelen, 1, stderr);
+//     fprintf(stderr, "%s", msg2);
+//   }
+
+//   static option::ArgStatus Unknown(const option::Option& option, bool msg)
+//   {
+//     if (msg) printError("Unknown option '", option, "'\n");
+//     return option::ARG_ILLEGAL;
+//   }
+
+//   static option::ArgStatus Required(const option::Option& option, bool msg)
+//   {
+//     if (option.arg != 0)
+//       return option::ARG_OK;
+
+//     if (msg) printError("Option '", option, "' requires an argument\n");
+//     return option::ARG_ILLEGAL;
+//   }
+
+//   static option::ArgStatus NonEmpty(const option::Option& option, bool msg)
+//   {
+//     if (option.arg != 0 && option.arg[0] != 0)
+//       return option::ARG_OK;
+
+//     if (msg) printError("Option '", option, "' requires a non-empty argument\n");
+//     return option::ARG_ILLEGAL;
+//   }
+
+//   static option::ArgStatus Numeric(const option::Option& option, bool msg)
+//   {
+//     char* endptr = 0;
+//     if (option.arg != 0 && strtol(option.arg, &endptr, 10)){};
+//     if (endptr != option.arg && *endptr == 0)
+//       return option::ARG_OK;
+
+//     if (msg) printError("Option '", option, "' requires a numeric argument\n");
+//     return option::ARG_ILLEGAL;
+//   }
+// };
+
+
 enum  optionIndex { UNKNOWN,
 		    START,
 		    END,
@@ -14,39 +60,31 @@ enum  optionIndex { UNKNOWN,
 		    YSTART,
 		    YEND,
 		    EXT,
+		    USING,
+		    EVERY,
 		    HELP
 };
 
 const option::Descriptor usage[] =
 {
-	{ UNKNOWN, 0,"" , ""    ,option::Arg::None,
+	{ UNKNOWN, 0,"" , ""    ,option::Arg::Optional,
 	 "USAGE: gnuplotter [options] filename_prefix\n\n"
 	 "Options:" },
-	{ START,    0,"" , "start",option::Arg::None,
-	 "  --start  \tStart time of plotting." },
-	{ END,    0,"" , "end",option::Arg::None,
-	 "  --end  \tEnd time of plotting." },
-	{ STEP,    0,"" , "step",option::Arg::None,
-	 "  --step  \tPlotting step." },
-	{ XSTART,    0,"" , "xstart",option::Arg::None,
-	 "  --xstart  \tStart of x axis." },
-	{ XEND,    0,"" , "xend",option::Arg::None,
-	 "  --xend  \tEnd of x axis." },
-	{ YSTART,    0,"" , "ystart",option::Arg::None,
-	 "  --ystart  \tStart of y axis." },
-	{ YEND,    0,"" , "yend",option::Arg::None,
-	 "  --yend  \tEnd of y axis." },
-	{ EXT,    0,"" , "ext",option::Arg::None,
-	 "  --ext  \tExtension config parameters for gnuplot." },
-	{ HELP,    0,"" , "help",option::Arg::None,
-	 "  --help  \tPrint usage and exit." },
-
-	// { UNKNOWN, 0,"" ,  ""   ,option::Arg::None,
-	//  "\nExamples:\n"
-	//  "  example --unknown -- --this_is_no_option\n"
-	//  "  example -unk --plus -ppp file1 file2\n" },
+	{ START,    0,"" , "start", option::Arg::Optional, "  --start  \tStart time of plotting." },
+	{ END,      0,"" , "end",   option::Arg::Optional, "  --end  \tEnd time of plotting." },
+	{ STEP,     0,"" , "step",  option::Arg::Optional, "  --step  \tPlotting step." },
+	{ XSTART,   0,"" , "xstart",option::Arg::Optional, "  --xstart  \tStart of x axis." },
+	{ XEND,     0,"" , "xend",  option::Arg::Optional, "  --xend  \tEnd of x axis." },
+	{ YSTART,   0,"" , "ystart",option::Arg::Optional, "  --ystart  \tStart of y axis." },
+	{ YEND,     0,"" , "yend",  option::Arg::Optional, "  --yend  \tEnd of y axis." },
+	{ EXT,      0,"" , "ext",   option::Arg::Optional, "  --ext  \tExtension config parameters for gnuplot." },
+	{ USING,    0,"" , "using", option::Arg::Optional, "  --using  \tUsing parameter for gnuplot." },
+	{ EVERY,    0,"" , "every", option::Arg::Optional, "  --every  \tEvery parameter for gnuplot." },
+	{ HELP,     0,"" , "help",  option::Arg::Optional, "  --help  \tPrint usage and exit." },
 	{ 0,0,0,0,0,0}
 };
+
+using namespace::std;
 
 // gnuplotter -ext "using 1:2 every ::0::6000" filename
 int main(int argc, char** argv)
@@ -57,19 +95,21 @@ int main(int argc, char** argv)
 	option::Parser parse(usage, argc, argv, options, buffer);
 
 	if (parse.error())
+	{
+		cout << "Parsing error" << endl;
 		return 1;
+	}
 
 	if (options[HELP] || argc == 0) {
 		option::printUsage(std::cout, usage);
 		return 0;
 	}
-	
-	// std::string s = std::string(argv[1]);
-	GNUPlotter gp( parse.nonOptionsCount() ? parse.nonOption(0) : "" );
 
 	for (option::Option* opt = options[UNKNOWN]; opt; opt = opt->next())
-		std::cout << "Unknown option: " << opt->name << "\n";
-	
+		std::cout << "Unknown option: " << opt->name << endl;
+
+	GNUPlotter gp( parse.nonOptionsCount() ? parse.nonOption(0) : "" );
+
 	if ( options[STEP] )
 		gp.set_step(atol(options[STEP].arg));
 
@@ -78,21 +118,27 @@ int main(int argc, char** argv)
 	
 	if ( options[END] )
 		gp.set_end(atol(options[END].arg));
-	
+
 	if ( options[XSTART] )
 		gp.set_xstart(atoi(options[XSTART].arg));
 	
 	if ( options[XEND] )
 		gp.set_xend(atoi(options[XEND].arg));
 	
-	if ( options[YEND] )
+	if ( options[YSTART] )
 		gp.set_ystart(atoi(options[YSTART].arg));
 	
 	if ( options[YEND] )
 		gp.set_yend(atoi(options[YEND].arg));
 
+	string s(" ");
+	if ( options[USING] )
+		s += string(" using ") + string(options[USING].arg);
+	if ( options[EVERY] )
+		s += string(" every ") + string(options[EVERY].arg);
 	if ( options[EXT] )
-		gp.set_ext(options[EXT].arg);
+		s += string(" ") + string(options[EXT].arg);
+	gp.set_ext(s);
 
-	//gp.plot();
+	gp.plot();
 }
