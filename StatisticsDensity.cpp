af7dc2a18970679b4b6cef83f94e0336ad7efe56
Jakub Palider <jpalider@gmail.com>
2014-05-17 17:57:46 +0200

    Header for output statistic file

--- StatisticsDensity.cpp
+++ StatisticsDensity.cpp
@@ -10,9 +10,15 @@
 #include "Vector.h"
 #include "Molecule.h"
 #include "Math.h"
+#include "Types.h"
 #include "tri_logger/tri_logger.hpp"
 
 using namespace std;
+
+namespace
+{
+	static const double MOLECULES_UM3_TO_U_MOLES = 2.2 / 1320;
+}
 
 StatisticsDensity::StatisticsDensity(Simulation *s, long interval) : Statistics(s, interval)
 {
@@ -20,21 +26,40 @@ StatisticsDensity::StatisticsDensity(Simulation *s, long interval) : Statistics(
 
 void StatisticsDensity::run(long time, const MStore* const molecules, const std::vector<Receptor>* const rcells)
 {
+	static bool first_run = true;
+
 	if (time % sinterval == 0)
 	{
 		const Vector* v = nullptr;
 		const double units_um = 0.001;
+
+		if (first_run)
+		{
+			for (vector<Receptor>::const_iterator cit = rcells->begin(); cit != rcells->end(); ++cit)
+			{
+				ofstream stat_stream;
+				stringstream ss;
+				ss << "_rcell_" << cit->id();
+
+				stat_stream.open(filename(ss.str()).c_str(), ios::app);
+				stat_stream << "# " << *cit << endl;
+				stat_stream << "# time molecule_count_per_volume molecule_count umoles" << endl;
+				stat_stream.close();
+			}
+			first_run = false;
+		}
+
 		for (vector<Receptor>::const_iterator cit = rcells->begin(); cit != rcells->end(); ++cit)
 		{
-			const double diameter   = cit->radius();
-			const double sqdiameter = diameter*diameter;
-			const double vol        = sphere_volume(diameter / sscale * units_um); // back to nm and then to um
+			const Coordinate diameter   = cit->radius();
+			const Coordinate sqdiameter = diameter*diameter;
+			const Coordinate vol        = sphere_volume(diameter / sscale * units_um); // back to nm and then to um
 			int m_count = 0;
 
 			for (auto it = molecules->begin(); it != molecules->end(); ++it)
 			{
 				v = (*it)->position();
-				double sqd = squared_distance_between_points(cit->position(), v);
+				Coordinate sqd = squared_distance_between_points(cit->position(), v);
 				if ( sqd < sqdiameter )
 				{
 					m_count++;
@@ -45,9 +70,12 @@ void StatisticsDensity::run(long time, const MStore* const molecules, const std:
 			stringstream ss;
 			ss << "_rcell_" << cit->id();
 			stat_stream.open(filename(ss.str()).c_str(), ios::app);
-			stat_stream << time * sscale << " " << ( m_count / vol ) << " " << m_count << endl;
+			stat_stream << time * sscale
+				    << " " << ( m_count / vol                      )
+				    << " " << ( m_count                            )
+				    << " " << ( m_count / MOLECULES_UM3_TO_U_MOLES )
+				    << endl;
 			stat_stream.close();
-
 		}
 	}
 }
